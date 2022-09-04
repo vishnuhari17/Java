1f95da9ac6c2aed85a84f57374892f39dc7e4cc0
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2022-09-04 23:16:37 +0530

    Added code

---
+++ com/vishnuhari17/basics/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.vishnuhari17.basics;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        //My First program
+        System.out.println("Hello World");
+    }
+}
