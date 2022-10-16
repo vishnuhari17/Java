29ae3be413d0bd11821caf5f4fa3a20b6b3edcb8
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2022-10-16 16:54:08 +0530

    Modified code

--- com/vishnuhari17/basics/switch_case.java
+++ com/vishnuhari17/basics/switch_case.java
@@ -1,7 +1,9 @@
 package com.vishnuhari17.basics;
 
+@SuppressWarnings("MismatchedStringCase")
 public class switch_case {
 
+    @SuppressWarnings("ConstantConditions")
     public static void main(String[] args) {
 
         //Switch is a statement that allows us to test the equality of a variable against a list of values
@@ -9,33 +11,24 @@ public class switch_case {
         String day = "pizza";
 
         switch (day){
-            case "Monday" :
-                System.out.println("It's a Monday");
-                break;
-            case "Tuesday":
-                System.out.println("It's a Tuesday");
-                break;
-            case "Wednesday" :
-                System.out.println("It's a Wednesday");
-                break;
-            case "Thursday" :
-                System.out.println("It's a Thursday");
-                break;
-            case "Friday" :
-                System.out.println("It's a Friday");
-                break;
-            case "Saturday" :
-                System.out.println("It's a Saturday");
-                break;
-            case "Sunday" :
-                System.out.println("It's a Sunday");
-                break;
-            default:
-                System.out.println("It's not a day! :/");
-
+            
+            case "Monday" : System.out.println("It's a Monday");
+            break;
+            case "Tuesday": System.out.println("It's a Tuesday");
+            break;
+            case "Wednesday" : System.out.println("It's a Wednesday");
+            break;
+            case "Thursday" : System.out.println("It's a Thursday");
+            break;
+            case "Friday" : System.out.println("It's a Friday");
+            break;
+            case "Saturday" : System.out.println("It's a Saturday");
+            break;
+            case "Sunday" : System.out.println("It's a Sunday");
+            break;
+            default: System.out.println("It's not a day! :/");
 
         }
-
 
     }
 }
