d47d22f67b3cb089b0ebea540e81313b0ada4ad9
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2022-10-16 17:16:52 +0530

    Added code

--- com/vishnuhari17/basics/logical_operators.java
+++ com/vishnuhari17/basics/logical_operators.java
@@ -1,9 +1,15 @@
 package com.vishnuhari17.basics;
 
+import java.util.Scanner;
+
 public class logical_operators {
+
+    /*Logical operators: && and
+                         || or
+                         ! not
+     */
     public static void main(String[] args) {
         int temp =10;
-
         if(temp>30){
             System.out.println("It's hot outside!");
         }
@@ -13,5 +19,22 @@ public class logical_operators {
         else{
             System.out.println("It's cold outside");
         }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("You are playing a game press q or Q to quit");
+        String responce = scanner.next();
+
+        if(responce.equals("q")||responce.equals("Q")){
+            System.out.println("You quited the game");
+        }
+        else {
+            System.out.println("You are still playing *pew pew*");
+        }
+
     }
 }
+
+
+
+
+
