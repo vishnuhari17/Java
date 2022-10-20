66b86d8a4bee3ea0ff9e3a12f720af3d9121e050
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2022-10-20 23:25:22 +0530

    Modified the code

---
+++ com/vishnuhari17/basics/for_loop.java
@@ -0,0 +1,12 @@
+package com.vishnuhari17.basics;
+
+public class for_loop {
+    public static void main(String[] args) {
+        for(int i=0;i<=10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Print it's over");
+    }
+}
+
+
